4bd7c2525475bb865fff191f6ff286fd970b05bd
Artem Borisenko <aprricod@gmail.com>
2022-04-20 00:39:51 +0200

    create courier test changed

--- src/test/java/ru/yandex/praktikum/CreateCourierTest.java
+++ src/test/java/ru/yandex/praktikum/CreateCourierTest.java
@@ -5,6 +5,7 @@ import io.qameta.allure.junit4.DisplayName;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import model.Courier;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -16,13 +17,6 @@ public class CreateCourierTest {
     @Before
     public void setUp() {
         RestAssured.baseURI = "https://qa-scooter.praktikum-services.ru/";
-    }
-
-    //создание курьера с неуникальным логином
-    @Test
-    @DisplayName("Create courier with non-unique login")
-    @Description("Unsuccessful create courier with non-unique login")
-    public void createNewCourier() {
 
         //создаем курьера
         Courier courierFirst = new Courier("testArtemCourier", "1234", "artemFirst");
@@ -36,17 +30,13 @@ public class CreateCourierTest {
         response.then().assertThat()
                 .body("ok", equalTo(true))
                 .and().statusCode(201);
+    }
 
-        //логинимся курьером с целью узнать id
-        Response login =
-                given()
-                        .header("Content-type", "application/json")
-                        .and()
-                        .body(courierFirst)
-                        .when()
-                        .post("/api/v1/courier/login");
-        int courierId = login.then().extract().body().path("id");
-        login.then().assertThat().body("id", notNullValue()).and().statusCode(200);
+    //создание курьера с неуникальным логином
+    @Test
+    @DisplayName("Create courier with non-unique login")
+    @Description("Unsuccessful create courier with non-unique login")
+    public void createNewCourier() {
 
         //пытаемся создать курьера с неуникальным логином
         Courier courierSecond = new Courier("testArtemCourier", "1234", "artemSecond");
@@ -60,6 +50,22 @@ public class CreateCourierTest {
         response1.then().assertThat()
                 .body("message", equalTo("Этот логин уже используется. Попробуйте другой."))
                 .and().statusCode(409);
+    }
+
+    @After
+    public void cleanUp() {
+        Courier courierFirst = new Courier("testArtemCourier", "1234");
+
+        //логинимся курьером с целью узнать id
+        Response login =
+                given()
+                        .header("Content-type", "application/json")
+                        .and()
+                        .body(courierFirst)
+                        .when()
+                        .post("/api/v1/courier/login");
+        int courierId = login.then().extract().body().path("id");
+        login.then().assertThat().body("id", notNullValue()).and().statusCode(200);
 
         //удаляем курьера
         Response delete =
